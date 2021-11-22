ca907ca67e757788bcb07cea56a8215d2b8c3083
tharpercp <tharpercp@gmail.com>
2021-11-22 14:14:39 -0800

    root is rendering

--- Pictr/frontend/components/app.jsx
+++ Pictr/frontend/components/app.jsx
@@ -1,9 +1,14 @@
 import SignupContainer from './session/signup_container';
 import { Route } from "react-router-dom";
+import { AuthRoute, ProtectedRoute } from '../utils/route_utils'
+import LoginContainer from './session/login_container';
 
 
 export default () => (
     <div>
-        <Route path="/signup" component={SignupContainer} />
+        <ProtectedRoute path="/feed" component={NavBarContainer} />
+        <ProtectedRoute path="/feed" component={PhotoContainer} />
+        <AuthRoute path="/login" component={LoginContainer} />
+        <AuthRoute path="/signup" component={SignupContainer} />
     </div>
 );
