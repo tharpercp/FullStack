2c46625d3a25e1d108d70f681f8cc4a264f7da06
tharpercp <tharpercp@gmail.com>
2021-12-03 09:16:01 -0800

    working on routing bug

--- frontend/reducers/albums_reducer.js
+++ frontend/reducers/albums_reducer.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { RECEIVE_USER_ALBUMS, RECEIVE_ALBUM } from '../actions/album_actions';
+// import React from 'react';
+// import { RECEIVE_USER_ALBUMS, RECEIVE_ALBUM } from '../actions/album_actions';
 
-const albumReducer = (oldState = {}, action) => {
-    Object.freeze(oldState);
-    let nextState = Object.assign({}, oldState);
+// const albumReducer = (oldState = {}, action) => {
+//     Object.freeze(oldState);
+//     let nextState = Object.assign({}, oldState);
 
-    switch (action.type) {
-        case RECEIVE_USER_ALBUMS:
-            return action.albums;
-        case RECEIVE_ALBUM:
-            nextState[action.album.id] = action.album;
-            return nextState;
-        default:
-            return oldState;
-    }
-};
+//     switch (action.type) {
+//         case RECEIVE_USER_ALBUMS:
+//             return action.albums;
+//         case RECEIVE_ALBUM:
+//             nextState[action.album.id] = action.album;
+//             return nextState;
+//         default:
+//             return oldState;
+//     }
+// };
 
-export default albumReducer;
+// export default albumReducer;
